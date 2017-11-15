7e08b8d0ef5afe7f34fbe557804e012d1ed40325
sfcpeters <32931928+sfcpeters@users.noreply.github.com>
2017-11-15 10:33:59 -0500

    Add files via upload

---
+++ M4HW1_DistanceTraveled_PetersM.py
@@ -0,0 +1,26 @@
+#CTI 110
+#M4HW1-Distance Traveled
+#Michael Peters
+#14 November 2017
+
+#this is a calculation of the distance a car will travel
+# at a certian speed for a specified time
+
+# the formula is distance = speed * time
+
+speed = 70
+
+distanceAfter6 = speed * 6
+
+distanceAfter10 = speed * 10
+
+distanceAfter15 = speed * 15
+
+
+
+
+print('The distance the car will travel in 6 hours at 70 mph is' ,distanceAfter6,'miles')
+
+print('The distance the car will travel in 10 hours at 70 mph is' ,distanceAfter10,'miles')
+
+print('The distance the car will travel in 15 hours at 70 mph is' ,distanceAfter15,'miles')
